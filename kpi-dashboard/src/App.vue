c7ba3187bed019cc19b8f2170608601efc741682
hadesxiong <fengshixiong890614@gmail.com>
2023-08-01 18:07:04 +0800

    add iconpark and babel-plugin-import package, update site-menu

--- kpi-dashboard/src/App.vue
+++ kpi-dashboard/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   <SideMenu></SideMenu>
 </template>
@@ -18,6 +18,7 @@ export default {
 </script>
 
 <style>
+@import url(./assets/style/update.css);
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- kpi-dashboard/src/components/main/side-menu.vue
+++ kpi-dashboard/src/components/main/side-menu.vue
@@ -1,9 +1,11 @@
 <template>
     <div>
-        <a-menu style="width: 300px;" mode="horizontal">
+        <a-menu style="width: 12vw; min-width: 220px;" mode="inline">
             <a-sub-menu v-for="item in menu_data" :key="item.menu_key">
+                <template #icon><icon-park :type=item.menu_icon theme="outline" size="16"></icon-park></template>
                 <template #title>{{ item.menu_title }}</template>
-                <!-- <a-menu-item></a-menu-item> -->
+                <a-menu-item v-for="sub_item in item.sub_menu" :key="sub_item.menu_key">{{ sub_item.menu_title
+                }}</a-menu-item>
             </a-sub-menu>
         </a-menu>
     </div>
@@ -11,16 +13,19 @@
 
 <script>
 import { defineComponent } from 'vue';
+import { IconPark } from "@icon-park/vue-next/es/all";
 
 export default defineComponent({
     name: 'SideMenu',
     // props: ['icon'],
-    components: {},
+    components: {
+        IconPark
+    },
     data() {
         this.menu_data = [
-            { menu_key: 'home', menu_title: '首页', menu_icon: 'home-outlined', sub_menu: [] },
-            { menu_key: 'dashboard', menu_title: '数据看板', menu_icon: 'dashboard-outlined', sub_menu: [{ menu_key: 'dashboard-important', menu_title: '重要指标分析', menu_icon: '' }] },
-            { menu_key: 'rank', menu_title: '业绩排行', menu_icon: 'number-outlined', sub_menu: [] }
+            { menu_key: 'dashboard', menu_title: '数据看板', menu_icon: 'DashboardOne', sub_menu: [{ menu_key: 'dashboard-important', menu_title: '重要指标分析', menu_icon: '' }, { menu_key: 'dashboard-other', menu_title: '同业指标分析', menu_icon: '' }] },
+            { menu_key: 'rank', menu_title: '业绩排行', menu_icon: 'NewComputer', sub_menu: [{ menu_key: 'rank-important', menu_title: '重要指标排行', menu_icon: '' }] },
+            { menu_key: 'table', menu_title: '数据报表', menu_icon: 'Notes', sub_menu: [{ menu_key: 'enterprise-table', menu_title: '企金数据报表', menu_icon: '' }, { menu_key: 'retail-table', menu_title: '零售数据报表', menu_icon: '' }, { menu_key: 'bank-table', menu_title: '同业数据报表', menu_icon: '' }, { menu_key: 'other-table', menu_title: '其他数据报表', menu_icon: '' }] }
         ];
         return {};
     },
