4f42bab3f102988e86e6ffff2fad1bc11e2fd97c
hadesxiong <fengshixiong890614@gmail.com>
2023-08-31 10:18:31 +0800

    adjust users fields

--- DemoServer/kpi_server/serializers.py
+++ DemoServer/kpi_server/serializers.py
@@ -4,12 +4,30 @@ from rest_framework import serializers
 
 # 引入model
 from kpi_server.models import Users
+from kpi_server.models import Org
 
 
 # 序列化
 class UsersSerializer(serializers.ModelSerializer):
 
+    # 关联获取机构相关信息
+    org_name = serializers.SerializerMethodField()
+    # org_manager = serializers.SerializerMethodField()
+
+    def get_org_name(self,obj):
+
+        org_obj = Org.objects.get(org_num=obj.user_belong_org)
+        return org_obj.org_name
+    
     class Meta:
 
         model = Users
-        fields = '__all__'
+        # fields = '__all__'
+        fields = ('notes_id','user_name','user_character','user_belong_group','org_name')
+
+class OrgSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model=Org
+        fields= '__all__'
--- DemoServer/kpi_server/views.py
+++ DemoServer/kpi_server/views.py
@@ -1,15 +1,32 @@
 from django.shortcuts import render
 
+from django.core.paginator import Paginator
+
 from django.http.response import JsonResponse
 from rest_framework.decorators import api_view
 
 from kpi_server.models import Users
 
 from kpi_server.serializers import UsersSerializer
+
+import math
 # Create your views here.
 
 @api_view(['GET'])
-def users_list(request):
+def getUsersList(request):
+
+    query_params = {
+        'belong_group': request.query_params.get('group',0),
+        'user_character': request.query_params.get('character',0),
+        'org_level': request.query_params.get('org',0),
+        'key_word':request.query_params.get('ext',''),
+        'user_client': request.query_params.get('client',None)
+    }
+
+    
+    users_data_all = Users.objects.prefetch_related().all()
+    # page_inator = Paginator(users_data_all,15)
+    # page_max = math.cell(len(users_data_all)/15)
 
     users_serializer = UsersSerializer(Users.objects.all(),many=True)
     re_msg = {'code':0,'data':users_serializer.data}
