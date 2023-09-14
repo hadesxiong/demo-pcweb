ce91962ec120e0305c3ecf97ff9624a3e091d4d7
hadesxiong <fengshixiong890614@gmail.com>
2023-09-14 21:24:50 +0800

    update

--- kpi-dashboard/vue.config.js
+++ kpi-dashboard/vue.config.js
@@ -1,12 +1,20 @@
 const { defineConfig } = require('@vue/cli-service');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CompressionPlugin = require('compression-webpack-plugin')
 
 module.exports = defineConfig({
   transpileDependencies: true,
   runtimeCompiler: true,
   configureWebpack: {
     plugins: [
-      new MiniCssExtractPlugin()
+      new MiniCssExtractPlugin(),
+      new CompressionPlugin({
+        test: /\.(js|css)?$/i,
+        algorithm: 'gzip',
+        filename: '[path][base].gz',
+        minRatio: 1,
+        deleteOriginalAssets: false
+      })
       // ...other plugins
     ],
   },
